541b29739c3a4b0924717566eafcd77906d2a8dc
Prerna <iprernasingh4@gmail.com>
2023-09-11 11:56:47 +0530

    first commit

---
+++ celebration.js
@@ -0,0 +1,12 @@
+//Day 100 of code challenge
+function completeCelebration() {
+  const achievement = "Completed 100DaysOfCode challenge!";
+  const excitementLevel = 100;
+
+  //Let's Celebrate
+  for (let i = 0; i < excitementLevel; i++) {
+    console.log(`${achievement}`);
+  }
+}
+
+completeCelebration();
